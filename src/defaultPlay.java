870a812a9671d5dd0325cec71e1e2c5e8f5397f9
Thai Le <thailekha@yahoo.com>
2015-03-04 18:51:07 +0700

    (Thai)	added default play

---
+++ src/defaultPlay.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class defaultPlay {
+
+	//private Scanner input = new Scanner(System.in);
+	private ArrayList<Player> players;
+	private ArrayList<Commodity> motorShares;
+	private ArrayList<Commodity> shippingShares;
+	private ArrayList<Commodity> steelShares;
+	private ArrayList<Commodity> storeShares;
+	private ArrayList<Card> cards;
+	
+	public defaultPlay()
+	{
+		players = new ArrayList<Player>();
+		motorShares = new ArrayList<Commodity>();
+		shippingShares = new ArrayList<Commodity>();
+		steelShares = new ArrayList<Commodity>();
+		storeShares = new ArrayList<Commodity>();
+	}
+}
