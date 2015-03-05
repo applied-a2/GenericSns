10a066bb96839451b0d76d522085a34f9ec452a9
Shane <Shane@10.20.21.34>
2015-03-05 16:53:27 +0000

    Updated buy and sell methods

--- src/DefaultPlay.java
+++ src/DefaultPlay.java
@@ -4,93 +4,82 @@ public class DefaultPlay {
 
 	private Scanner input = new Scanner(System.in);
 	private ArrayList<Player> players;
-	private ArrayList<Commodity> motorShares;
-	private ArrayList<Commodity> shippingShares;
-	private ArrayList<Commodity> steelShares;
-	private ArrayList<Commodity> storeShares;
+	private ArrayList<Commodity> shares;
 	private ArrayList<Card> cards;
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		DefaultPlay defaultPlay = new DefaultPlay();
 		defaultPlay.display();
 	}
-	
-	public DefaultPlay()
-	{
+
+	public DefaultPlay() {
 		players = new ArrayList<Player>();
-		motorShares = new ArrayList<Commodity>();
-		shippingShares = new ArrayList<Commodity>();
-		steelShares = new ArrayList<Commodity>();
-		storeShares = new ArrayList<Commodity>();
+		shares = new ArrayList<Commodity>();
 		setUpShares();
-		cards = new ArrayList<Card>(); //(Thai): I forgot this line, sorry ^^
-		//cards.addAll(getMotorCards());
-		//cards.addAll(getShippingCards());
-		//cards.addAll(getSteelCards());// changed to steel
-		//cards.addAll(getStoreCards());
+		cards = new ArrayList<Card>(); // (Thai): I forgot this line, sorry ^^
+		// cards.addAll(getMotorCards());
+		// cards.addAll(getShippingCards());
+		// cards.addAll(getSteelCards());// changed to steel
+		// cards.addAll(getStoreCards());
 		setUpCards();
 	}
-	
-	public void display()
-	{
+
+	public void display() {
 		System.out.println("Setup complete, On screen !");
-		System.out.println("Number of shares: " + motorShares.size() + " " + motorShares.get(0).getCommodityType()
-				+ "\n" + shippingShares.size() + " " + shippingShares.get(0).getCommodityType()
-				+ "\n" + steelShares.size() + " " + steelShares.get(0).getCommodityType()
-				+ "\n" + storeShares.size() + " " + storeShares.get(0).getCommodityType());
-		String[] mainTypes = {"motors", "stores" ,"steels", "shippings"};
-		for(String mainType: mainTypes)
-		{
+		System.out.println("Number of shares: " + shares.size() );
+		String[] mainTypes = { "motors", "stores", "steels", "shippings" };
+		for (String mainType : mainTypes) {
 			int count = 0;
-			for(Card card: cards)
-			{
-				if(mainType.equals(card.getCardType()))
-				{
+			for (Card card : cards) {
+				if (mainType.equals(card.getCardType())) {
 					count++;
 				}
 			}
 			System.out.println(count + " " + mainType + " cards");
 		}
 	}
-	
-	public void setUpCards()
-	{
-		String[] mainTypes = {"motors", "stores" ,"steels", "shippings"}; // a primitive array list containing 4 main types of cards
-		for(String mainType: mainTypes) //loop through the list, so we don't have to declare every type 
+
+	public void setUpCards() {
+		String[] mainTypes = { "motors", "stores", "steels", "shippings" }; // a
+																			// primitive
+																			// array
+																			// list
+																			// containing
+																			// 4
+																			// main
+																			// types
+																			// of
+																			// cards
+		for (String mainType : mainTypes) // loop through the list, so we don't
+											// have to declare every type
 		{
-			for(int upValue = 2; upValue <= 4; upValue++)	//generates the "up" cards, values (2,3,4) will control this loop
+			for (int upValue = 2; upValue <= 4; upValue++) // generates the "up"
+															// cards, values
+															// (2,3,4) will
+															// control this loop
 			{
 				cards.add(new Card(mainType, "up", upValue));
 			}
-			for(int downValue = 2; downValue <= 4; downValue++) //same as above, generates the "down" cards
+			for (int downValue = 2; downValue <= 4; downValue++) // same as
+																	// above,
+																	// generates
+																	// the
+																	// "down"
+																	// cards
 			{
 				cards.add(new Card(mainType, "down", downValue));
 			}
 		}
-		
-		cards.add(new Card("bull", "up", 4));  //add the 2 bonus cards
+
+		cards.add(new Card("bull", "up", 4)); // add the 2 bonus cards
 		cards.add(new Card("bear", "down", 4));
 	}
-	
-	public void setUpShares()
-	{
-	while (motorShares.size() < 27){
-		Commodity motors = new Commodity("motors", 10);
-		motorShares.add(motors);
-	}
-	while (shippingShares.size() < 27){
-		Commodity shipping = new Commodity("shipping", 10);
-		shippingShares.add(shipping);
-	}
-	while (steelShares.size() < 27){
-		Commodity steel = new Commodity("steel", 10);
-		steelShares.add(steel);
-	}
-	while (storeShares.size() < 27){
-		Commodity store = new Commodity("store", 10);
-		storeShares.add(store);
-	}
-	 
-	}
+
+	public void setUpShares() {
+		while (shares.size() < 27) {
+			Commodity motors = new Commodity("motors", 10);
+			shares.add(motors);
+		}
 }
+
+	}
--- src/Player.java
+++ src/Player.java
@@ -23,11 +23,41 @@ public class Player {
 		return holdingCard;
 	}
 	
-	public void buyShares(ArrayList<Commodity> shares)
-	{
-		shareHolds = shares;
+	public ArrayList<Commodity> buyShares(String commodityTypeToBeBought, int shareNum)
+	{ 
+		int moneyToPay = 0;
+		ArrayList<Commodity> sharesToBeBought = new ArrayList<Commodity>();
+		for(Commodity commodity: shareHolds)
+		{
+			if(commodity.getCommodityType().equals(commodityTypeToBeBought)&&(shareNum>0))
+			{
+				moneyToPay += commodity.getValue();
+				shareHolds.add(commodity);//Adds commodity to the players stack
+				sharesToBeBought.add(commodity);
+				shareNum--;//For loop will run until numShares reaches 0
+			}
+		}
+		money = money - moneyToPay;//Player pays money
+		return sharesToBeBought;//Being returned so it will be taken away from the array as the array will be minus cards
 	}
 	
 	/*A method to sell share(s) from the shares array list
 	 */
+	public ArrayList<Commodity> sellShares(String commodityTypeToBeSold, int numShares)//The commodity type and the number of shares the player wants to sell
+	{
+		int moneyToReceive= 0;
+		ArrayList<Commodity> sharesToBeSold = new ArrayList<Commodity>();
+		for(Commodity commodity: shareHolds)
+		{
+			if(commodity.getCommodityType().equals(commodityTypeToBeSold)&&(numShares>0))
+			{
+				moneyToReceive += commodity.getValue();//For each time the loop runs, the value of that commodity will be added to moneyToPay
+				shareHolds.remove(commodity);//Takes commodity from the players stack
+				sharesToBeSold.add(commodity);
+				numShares--;//For loop will run until numShares reaches 0
+			}
+		}
+		money = money + moneyToReceive;//player receives money
+		return sharesToBeSold;//Being returned as it will be called upon by the DefaultPlay Class and added to the Array
+	}
 }
