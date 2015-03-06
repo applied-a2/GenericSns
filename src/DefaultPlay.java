1b51f430d92e92b4c35284b20bbccbaf5535c8d1
Shane <Shane@10.20.21.34>
2015-03-06 12:33:05 +0000

    (Shane) buy and sell methods + made all shares into one array

--- src/DefaultPlay.java
+++ src/DefaultPlay.java
@@ -9,10 +9,6 @@ public class DefaultPlay {
 	private ArrayList<Player> players;
 	private ArrayList<Commodity> shares;
 	private ArrayList<Card> cards;
-<<<<<<< HEAD
-
-	public static void main(String[] args) {
-=======
 	private Random randomCard;
 	
 	
@@ -20,7 +16,6 @@ public class DefaultPlay {
 	
 	public static void main(String[] args)
 	{
->>>>>>> c8f511b6a5c2b3834a697145a932ad70a7906ddf
 		DefaultPlay defaultPlay = new DefaultPlay();
 		defaultPlay.display();
 	}
@@ -29,20 +24,17 @@ public class DefaultPlay {
 		players = new ArrayList<Player>();
 		shares = new ArrayList<Commodity>();
 		setUpShares();
-<<<<<<< HEAD
 		cards = new ArrayList<Card>(); // (Thai): I forgot this line, sorry ^^
 		// cards.addAll(getMotorCards());
 		// cards.addAll(getShippingCards());
 		// cards.addAll(getSteelCards());// changed to steel
 		// cards.addAll(getStoreCards());
-=======
 		randomCard = new Random();
 		cards = new ArrayList<Card>(); //(Thai): I forgot this line, sorry ^^
 		//cards.addAll(getMotorCards());
 		//cards.addAll(getShippingCards());
 		//cards.addAll(getSteelCards());// changed to steel
 		//cards.addAll(getStoreCards());
->>>>>>> c8f511b6a5c2b3834a697145a932ad70a7906ddf
 		setUpCards();
 	}
 
@@ -96,7 +88,6 @@ public class DefaultPlay {
 		cards.add(new Card("bull", "up", 4)); // add the 2 bonus cards
 		cards.add(new Card("bear", "down", 4));
 	}
-<<<<<<< HEAD
 
 	public void setUpShares() {
 		while (shares.size() < 27) {
@@ -105,7 +96,6 @@ public class DefaultPlay {
 		}
 	}
 
-=======
 	
 
 	public int generateRandomCard()
@@ -115,31 +105,4 @@ public class DefaultPlay {
         
         return index;
     }
->>>>>>> c8f511b6a5c2b3834a697145a932ad70a7906ddf
-	
-	
-	        
-	   
-	
-
-
-	public void setUpShares()
-	{
-		while (motorShares.size() < 27){
-			Commodity motors = new Commodity("motors", 10);
-			motorShares.add(motors);
-		}
-		while (shippingShares.size() < 27){
-			Commodity shipping = new Commodity("shipping", 10);
-			shippingShares.add(shipping);
-		}
-		while (steelShares.size() < 27){
-			Commodity steel = new Commodity("steel", 10);
-			steelShares.add(steel);
-		}
-		while (storeShares.size() < 27){
-			Commodity store = new Commodity("store", 10);
-			storeShares.add(store);
-		}
-	}
 }
