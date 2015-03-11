33e43d4da202f0896dbb0301e5d0495369eccbfb
Peter Hearne <hearne73@gmail.com>
2015-03-11 15:58:34 +0000

    (pete)added to turn statement in DefaultPlay

--- src/DefaultPlay.java
+++ src/DefaultPlay.java
@@ -7,7 +7,7 @@ public class DefaultPlay {
 	private Shares shares;
 	private ArrayList<Card> cards;
 	private String[] mainTypes = { "motors", "stores", "steels", "shippings" }; //just loop through this list if needed
-	
+	private int turn = 0;
 	public static void main(String[] args)
 	{
 		DefaultPlay defaultPlay = new DefaultPlay();
@@ -82,7 +82,14 @@ public class DefaultPlay {
 			System.out.println("--------------------");
 			if(round != 1)
 			{
-				Collections.reverse(players);
+				//int limit = players.size();?????
+				while (turn > 0){
+				turn = (turn--);// % limit; ??????
+				}	}
+				else {
+					turn = players.size();
+				}
+			//	Collections.reverse(players);
 			}
 			System.out.println("Round " + round);
 			
