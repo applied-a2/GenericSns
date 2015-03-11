a02072f6db1dc2fda6f223d10591b6f05f19e931
Peter Hearne <hearne73@gmail.com>
2015-03-11 20:12:27 +0000

    (pete) actully fixed the turn thing

--- src/DefaultPlay.java
+++ src/DefaultPlay.java
@@ -82,10 +82,8 @@ public class DefaultPlay {
 			System.out.println("--------------------");
 			if(round != 1)
 			{
-				int limit = players.size();
-				turn = (turn++) % limit;// ??????
-				
-			//	Collections.reverse(players);
+				int turn = players.size();			
+				Collections.rotate(players,turn-1);
 			}
 			System.out.println("Round " + round);
 			
